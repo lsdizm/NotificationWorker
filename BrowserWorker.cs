edc1435d74ae2816cb134dba7c3541e1ac1af771
burn7 <burn7@ezcaretech.com>
2023-03-10 15:27:11 +0900

    init2

---
+++ BrowserWorker.cs
@@ -0,0 +1,78 @@
+﻿using CefSharp;
+using CefSharp.OffScreen;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationWorker
+{
+    public class BrowserWorker
+    {
+        private ChromiumWebBrowser _chrome;
+
+        public async Task<string> GetSchedule()
+        {
+            var result = await MakeSession().ConfigureAwait(false);
+
+            return result;
+        }
+
+        private async Task<string> MakeSession()
+        {
+            //쿠키 데이터 사용하는 방법
+            CefSettings settings = new CefSettings();
+            settings.CachePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\CEF";
+            CefSharp.Cef.Initialize(settings);
+
+            //웹 사이트 이동
+            var url = "https://gwa.ezcaretech.com";
+            _chrome = new ChromiumWebBrowser(url);
+            //_chrome.async
+            var loadResult = await _chrome.LoadUrlAsync(url).ConfigureAwait(false);
+            if (loadResult.Success)
+            {
+                //_chrome.ExecuteScriptAsync("document.getElementById('userId').value = 'burn7';");
+                //_chrome.ExecuteScriptAsync("document.getElementById('userPw').value = '!dlwlzpdjxpr1';");
+                //_chrome.ExecuteScriptAsync("actionLogin();");
+
+                var result = await _chrome.GetSourceAsync().ConfigureAwait(false);
+                return result;
+
+
+            }
+
+            else
+            {
+                return "fail";
+            }
+            //_chrome = new ChromiumWebBrowser("https://gwa.ezcaretech.com");
+            //한국어 설정
+            //_chrome.BrowserSettings.AcceptLanguageList = "ko-KR";
+
+            //페이지 로딩 완료 이벤트            
+            //_chrome.LoadingStateChanged += OnLoadingStateChanged;
+
+        }
+
+
+
+        private void OnLoadingStateChanged(object? sender, LoadingStateChangedEventArgs e)
+        {
+            _chrome.ExecuteScriptAsync("document.getElementById('userId').value = 'burn7';");
+            _chrome.ExecuteScriptAsync("document.getElementById('userPw').value = '!dlwlzpdjxpr1';");
+            _chrome.ExecuteScriptAsync("actionLogin();");
+
+            // 소스 조회
+            var resultHtml = string.Empty;
+            _chrome.GetSourceAsync().ContinueWith(result =>
+            {
+                resultHtml = result.Result;
+            });
+
+            resultHtml = string.Empty;
+        }
+    }
+}
--- Worker.cs
+++ Worker.cs
@@ -1,14 +1,17 @@
 using Newtonsoft;
+using System.ComponentModel.DataAnnotations;
 
 namespace NotificationWorker;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly BrowserWorker _browserWorker;
 
     public Worker(ILogger<Worker> logger)
     {
         _logger = logger;
+        _browserWorker = new BrowserWorker();
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -16,40 +19,42 @@ public class Worker : BackgroundService
         while (!stoppingToken.IsCancellationRequested)
         {
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await SearchCancelAppointmentSlot();
+            // await SearchCancelAppointmentSlot();
+            var result = await _browserWorker.GetSchedule().ConfigureAwait(false);
+            await SendMessage(result).ConfigureAwait(false);
             await Task.Delay(1000 * 60 * 60, stoppingToken);
         }
     }
 
-    private async Task SearchCancelAppointmentSlot()
-    {
-        var date = "20230327";
-        var url = string.Format(@"https://camp.xticket.kr/Web/Book/GetBookProduct010001.json?product_group_code=0003&start_date={0}&end_date={0}&book_days=1&two_stay_days=0&shopCode=212820734901", 
-                                date);
-        var httpClient = new HttpClient();
+    //private async Task SearchCancelAppointmentSlot()
+    //{
+    //    var date = "20230327";
+    //    var url = string.Format(@"https://camp.xticket.kr/Web/Book/GetBookProduct010001.json?product_group_code=0003&start_date={0}&end_date={0}&book_days=1&two_stay_days=0&shopCode=212820734901", 
+    //                            date);
+    //    var httpClient = new HttpClient();
 
-        try
-        {
-            var response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
-            var result = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+    //    try
+    //    {
+    //        var response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
+    //        var result = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
 
-            using var streamReader = new StreamReader(contentStream);
-            using var jsonReader = new JsonTextReader(streamReader);
-            
-            await SendMessage(jsonReader.ToString());
-        }
-        catch (Exception ex)
-        {
-            await SendMessage(ex.ToString());
-        }
+    //        using var streamReader = new StreamReader(contentStream);
+    //        using var jsonReader = new JsonTextReader(streamReader);
 
-    }
+    //        await SendMessage(jsonReader.ToString());
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        await SendMessage(ex.ToString());
+    //    }
+
+    //}
 
     private async Task<string> SendMessage(string message)
     {
         var token = "5974273292:AAH_dQslxH-pj78N-PffAwkYoVmbnPtj3bM";
         var chatId = "415767607";
-        var url = string.Format(@"https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}", 
+        var url = string.Format(@"https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}",
                                 token, chatId, message);
         var httpClient = new HttpClient();
 
